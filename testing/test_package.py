848399021c2c49fbf2f502982e41200e9c253050
Tianshuang(Ethan) Qiu <ethantqiu@berkeley.edu>
2023-08-14 15:49:40 -0700

    transmission work

--- testing/test_package.py
+++ testing/test_package.py
@@ -2,7 +2,8 @@ from urdfpy import URDF
 import numpy as np
 
 robot = URDF.load("ur5/ur_with_gripper.xacro")
-robot.show()
+print(robot.actuated_joint_names)
+robot.show(np.array([0, 0, 0, 0, 0, 0, 0.5]))
 # robot.animate(
 #     cfg_trajectory={
 #         "shoulder_pan_joint": [-np.pi / 4, np.pi / 4],
