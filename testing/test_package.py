7e21b0e69dc140744b88439dccc0822d615318cf
Tianshuang(Ethan) Qiu <ethantqiu@berkeley.edu>
2023-09-21 10:27:47 -0700

    visualizer work completed

--- testing/test_package.py
+++ testing/test_package.py
@@ -1,9 +1,30 @@
 import numpy as np
 import kinpy as kp
+from urdfpy import URDF
+import pdb
 
-robot = kp.build_chain_from_urdf(open("ur5/ur5.urdf").read())
-print(robot)
-print(robot.forward_kinematics(np.array([0, 0, 0, 0, 0, 0.5])))
+# robot = kp.build_chain_from_urdf(open("ur5/ur_with_gripper.xacro").read())
+robot = URDF.load("ur5/ur_with_gripper.xacro")
+# print(robot.get_joint_parameter_names())
+fk = robot.link_fk(
+    np.array(
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0.5,
+            0,
+        ]
+    )
+)
+print(list(fk))
+k = list(fk.keys())
+for kk in k:
+    print(kk.name)
+# pdb.set_trace()
+
 # robot.animate(
 #     cfg_trajectory={
 #         "shoulder_pan_joint": [-np.pi / 4, np.pi / 4],
